0bdb8afc7a0e2b404ee243f5f9a717180fcd90d7
Vineet Parmar <vineet.parmar@spit.ac.in>
2023-02-11 21:19:33 +0530

    some changes
    Co-authored-by: Hitstar53 <Hitstar53@users.noreply.github.com>

--- OCL B/ocl/ocl/settings.py
+++ OCL B/ocl/ocl/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -51,11 +52,13 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'ocl.urls'
-STATIC_URL = '/static/' 
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
