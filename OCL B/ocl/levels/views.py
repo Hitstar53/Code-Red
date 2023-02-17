aacf5f964ddeee8ea0e2152767eb2db72004901f
Vineet Parmar <vineet.parmar@spit.ac.in>
2023-02-17 17:51:27 +0530

    Full Backend.
    Co-authored-by: Anish <Anish241@users.noreply.github.com>
    Co-authored-by: Hitstar53 <Hitstar53@users.noreply.github.com>
    Co-authored-by: Omkar0512 <Acdbb@users.noreply.github.com>

--- OCL B/ocl/levels/views.py
+++ OCL B/ocl/levels/views.py
@@ -12,3 +12,37 @@ def level2(request):
 
 def level3(request):
     return render(request, 'levels/level3.html')
+
+def level4(request):
+    return render(request, 'levels/level4.html')
+
+def level5(request):
+    return render(request, 'levels/level5.html')
+
+def level6(request):
+    return render(request, 'levels/level6.html')
+
+def prelevel1(request):
+    return render(request, 'levels/pre_level1.html')
+
+def prelevel2(request):
+    return render(request, 'levels/pre_level2.html')
+
+def prelevel3(request):
+    return render(request, 'levels/pre_level3.html')
+
+def prelevel4(request):
+    return render(request, 'levels/pre_level4.html')
+
+def prelevel5(request):
+    return render(request, 'levels/pre_level5.html')
+
+def prelevel6(request):
+    return render(request, 'levels/pre_level6.html')
+
+def prebomb(request):
+    return render(request, 'levels/prebomb.html')
+
+def bomb(request):
+    return render(request, 'levels/bomb.html')
+
