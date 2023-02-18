a4812af67a798b3aedb8c5e6ea7e0ed10ecf43d7
Hatim Sawai <hatimsawai76@gmail.com>
2023-02-18 10:14:09 +0530

    updated routes and views
    Signed-off-by: Hatim Sawai <hatimsawai76@gmail.com>

--- OCL B/ocl/levels/urls.py
+++ OCL B/ocl/levels/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('level4/', views.level4, name='level4'),
     path('level5/', views.level5, name='level5'),
     path('level6/', views.level6, name='level6'),
-    path('level7/', views.level7, name='bomb'),
+    path('level7/', views.level7, name='level7'),
     path('prelevel1/', views.prelevel1, name='prelevel1'),
     path('prelevel2/', views.prelevel2, name='prelevel2'),
     path('prelevel3/', views.prelevel3, name='prelevel3'),
@@ -16,5 +16,5 @@ urlpatterns = [
     path('prelevel5/', views.prelevel5, name='prelevel5'),
     path('prelevel6/', views.prelevel6, name='prelevel6'),
     path('prelevel6/', views.prelevel6, name='prelevel6'),
-    path('prelevel7/', views.prelevel7, name='prebomb'),
+    path('prelevel7/', views.prelevel7, name='prelevel7'),
 ]
